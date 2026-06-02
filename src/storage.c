b467828d9eb2bc936bcd32f2f291f92690180f2a
s2505 <s2505@oita.kosen-ac.jp>
2026-06-02 09:36:38 +0900

    branch作成

--- src/storage.c
+++ src/storage.c
@@ -1,62 +1,38 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
+// storage.c
 #include "storage.h"
+#include <stdio.h>
 
-int load_cards(const char *filename, Card cards[]) {
-
-    FILE *fp = fopen(filename, "r");
-    if (!fp) {
-        perror("open failed");
-        return 0;
-    }
-
-    char line[512];
-    int count = 0;
-
-    // header skip
-    fgets(line, sizeof(line), fp);
-
-    while (fgets(line, sizeof(line), fp)) {
-
-        line[strcspn(line, "\n")] = '\0';
-
-        char *token = strtok(line, ",");
-        if (token) strcpy(cards[count].question, token);
-
-        token = strtok(NULL, ",");
-        if (token) strcpy(cards[count].answer, token);
-
-        token = strtok(NULL, ",");
-        if (token) cards[count].level = atoi(token);
-
-        count++;
-
-        if (count >= MAX_CARDS) break;
-    }
-
-    fclose(fp);
-    return count;
-}
-
-int save_cards(const char *filename, Card cards[], int count) {
-
+int save_cards(const char *filename, Card cards[], int count)
+{
     FILE *fp = fopen(filename, "w");
-    if (!fp) {
-        perror("save failed");
-        return 0;
-    }
-
-    fprintf(fp, "question,answer,level\n");
+    if (!fp) return 0;
 
     for (int i = 0; i < count; i++) {
-        fprintf(fp, "%s,%s,%d\n",
-            cards[i].question,
-            cards[i].answer,
-            cards[i].level);
+        fprintf(fp, "%s|%s|%d\n",
+                cards[i].question,
+                cards[i].answer,
+                cards[i].level);
     }
 
     fclose(fp);
     return 1;
+}
+
+int load_cards(const char *filename, Card cards[])
+{
+    FILE *fp = fopen(filename, "r");
+    if (!fp) return 0;
+
+    int count = 0;
+
+    while (count < MAX_CARDS &&
+           fscanf(fp, "%255[^|]|%255[^|]|%d\n",
+                  cards[count].question,
+                  cards[count].answer,
+                  &cards[count].level) == 3) {
+        count++;
+    }
+
+    fclose(fp);
+    return count;
 }
